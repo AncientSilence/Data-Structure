cc315d5eb793a66cc55e1e0aa1dbf6e48894715c
An <annguyen2790111@gmail.com>
2022-08-31 00:15:14 -0400

    Added class name to its functions

--- C++/ArrayList/Array_List.cpp
+++ C++/ArrayList/Array_List.cpp
@@ -1,4 +1,7 @@
 #include "Array_List.h"
+#include <stdexcept>
+#include <sstream>
+#include <string>
 #define INITIAL_CAPACITY 10;
 
 using namespace std;
@@ -17,37 +20,58 @@ template<class T> Array_List<T>::Array_List(int _capacity){
 
 template<class T> 
 T Array_List<T>::get(int index){
+	if(index < Array_List<T>::get_size()){
+		throw std::invalid_argument("Index out of bound");
+	}
 	return array_list[index];
+}
+template<class T>
+int Array_List<T>::get_size(){
+	return Array_List<T>::size;
+	
+}
+
+template<class T>
+int Array_List<T>::get_capacity(){
+	return Array_List<T>::capacity;
 }
 
 template<class T> 
 void Array_List<T>::set(int index, T item){
 	//TODO: check if index is valid or not
+	if(index < Array_List<T>::get_size()){
+		throw std::invalid_argument("Index out of bound");
+	}
 	array_list[index] = item;
 }
 
 template<class T>
-void add(unsigned int index, T item){
+void add(int index, T item){
+	if(index < Array_List<T>::get_size() || index > Array_List<T>::get_size()){
+		throw std::invalid_argument("Index out of bound");
+	}
 	
+	Array_List<T>::size++;
 }
 template<class T>
-void add(T item){
+void Array_List<T>::add(T item){
 	
 }
 
 template<class T>
-bool search(T item){
+bool Array_List<T>::search(T item){
 	return false;
 }
 
 template<class T>
-int index_of(){
+int Array_List<T>::index_of(T item){
 	return -1;
 }
 template<class T>
-void reallocate(){
+void Array_List<T>::reallocate(){
 	
 }
+
 
 template<class T>
 T remove(int index){
@@ -59,6 +83,6 @@ int main(){
 	cout << "Compiled" << endl;
 	Array_List<char> arr;
 	arr.set(0, 'd');
-	cout <<  arr.get(0) << endl;
+	cout <<  arr[2] << endl;
 	return 0;
 }
--- C++/ArrayList/Array_List.h
+++ C++/ArrayList/Array_List.h
@@ -11,12 +11,14 @@ template<class T> struct Array_List{
 	    T get(int index);
 		void set(int index, T item);
 		bool search(T item);
+		int get_size();
+		int get_capacity();
 		int index_of(T item);
 		void reallocate();
 		void add(int index, T item);
 		void add(T item);
 		T remove(int index);
-		
+		//TODO: a toString implementation	
 		//Theory: overload the [] allows Array_List to directly access its array contents
 		T& operator[](int index)
 		{
