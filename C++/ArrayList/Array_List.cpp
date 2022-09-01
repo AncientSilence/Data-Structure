63071ad5ecd2d238199064758ee0cfd50f4bc771
An <annguyen2790111@gmail.com>
2022-09-01 01:00:05 -0400

    Update Templates for Array List

--- C++/ArrayList/Array_List.cpp
+++ C++/ArrayList/Array_List.cpp
@@ -4,20 +4,36 @@
 using namespace std;
 
 template<class T> Array_List<T>::Array_List(){
-	size = 0;
-	capacity = INITIAL_CAPACITY;
-	array_list = new T[capacity];
+	Array_List<T>::size = 0;
+	Array_List<T>::capacity = INITIAL_CAPACITY;
+	Array_List<T>::array_list = new T[capacity];
 }
 
 template<class T> Array_List<T>::Array_List(int _capacity){
-	size = 0;
-	capacity = _capacity;
-	array_list = new T[capacity];
+	Array_List<T>::size = 0;
+	Array_List<T>::capacity = _capacity;
+	Array_List<T>::array_list = new T[capacity];
+}
+template<class T>Array_List<T>::~Array_List(){
+	Array_List<T>::size = 0;
+	Array_List<T>::capacity = 0;
+	delete Array_List<T>::array_list;
 }
 
 template<class T> 
 T Array_List<T>::get(int index){
+
 	return array_list[index];
+}
+template<class T>
+int Array_List<T>::get_size(){
+	return -1;
+	
+}
+
+template<class T>
+int Array_List<T>::get_capacity(){
+	return -1;
 }
 
 template<class T> 
@@ -27,38 +43,37 @@ void Array_List<T>::set(int index, T item){
 }
 
 template<class T>
-void add(unsigned int index, T item){
+void Array_List<T>::add(int index, T item){
 	
 }
 template<class T>
-void add(T item){
+void  Array_List<T>::add(T item){
 	
 }
 
-template<class T>
-bool search(T item){
-	return false;
-}
+
 
 template<class T>
-int index_of(){
+int Array_List<T>::search(T item){
 	return -1;
 }
 template<class T>
-void reallocate(){
+void Array_List<T>::reallocate(){
 	
 }
 
 template<class T>
-T remove(int index){
+T Array_List<T>::remove(int index){
 	return nullptr;
 }
 
+template<class T>
+void Array_List<T>::print(){
+
+}
 
 int main(){
-	cout << "Compiled" << endl;
-	Array_List<char> arr;
-	arr.set(0, 'd');
-	cout <<  arr.get(0) << endl;
+
+
 	return 0;
 }
--- C++/ArrayList/Array_List.h
+++ C++/ArrayList/Array_List.h
@@ -8,15 +8,17 @@ template<class T> struct Array_List{
 	public:
 		Array_List();
 		Array_List(int capacity);
+		~Array_List();
 	    T get(int index);
 		void set(int index, T item);
-		bool search(T item);
-		int index_of(T item);
+		int search(T item);
+		int get_size();
+		int get_capacity();
 		void reallocate();
 		void add(int index, T item);
 		void add(T item);
 		T remove(int index);
-		
+		void print();	
 		//Theory: overload the [] allows Array_List to directly access its array contents
 		T& operator[](int index)
 		{
