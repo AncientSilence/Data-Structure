22424a8b392af9bb7ebf5074a202d997f05677a9
AncientSilence <imhereforfreestuffs@gmail.com>
2022-09-09 22:07:00 -0400

    Implement singly linked list for java

--- Java/LinkedList/SinglyLinkedList.java
+++ Java/LinkedList/SinglyLinkedList.java
@@ -23,45 +23,124 @@ public class SinglyLinkedList<E>{
 		this.size = 0;
 	}
 	
-	public E get(int index){
-		return null;
+	public Node<E> get_head(){
+		return this.head;
 	}
 	
-	public void set(int index){
-		
+	public Node<E> get_node(int index){
+		Node<E> temp = this.head;
+		for(int i = 0; i < index; i++){
+			temp = temp.next;
+		}
+		return temp;
 	}
-	public Node get_node(int index){
-		return null;
+	
+	public E get(int index){
+		if(index < 0 || index > this.size){
+			throw new IndexOutOfBoundsException("Invalid index");
+		}
+		return this.get_node(index).item;
 	}
+	
+	public void set(int index, E item){
+		if(index < 0 || index > this.size){
+			throw new IndexOutOfBoundsException("Invalid index");
+		}
+		this.get_node(index).item = item;
+	}
+	
 	public int get_size(){
 		return this.size;
 	}
 	
-	public int exists(){
-		return -1;
-	}
-	public void add(E item){
+	public int exists(E item){
+		Node<E> temp = this.head;
+		int index = 0;
+		while(temp != null){
+			if(temp.item.equals(item)){
+				return index;
+			}
+			++index;
+			temp = temp.next;
+		}
 		
+		return -1;
 	}
 	
 	public void add(int index, E item){
-		
+		if(index < 0 || index > this.size){
+			throw new IndexOutOfBoundsException("Index is out of bound");
+		}
+		Node<E> holder = new Node<>(item);
+		if(this.size == 0){
+			holder.next = this.head;
+			this.head = holder;
+		}else{
+			Node<E> before = this.get_node(index - 1);
+			holder.next = before.next;
+			before.next = holder;
+		}
+		this.size++;
 	}
+	
+	public void add(E item){
+		this.add(this.size, item);
+	}
+	
 	
 	public E remove(int index){
-		return null;
+		if(index < 0 || this.size == 0 || index > this.size){
+			throw new IndexOutOfBoundsException("Index is out of bound or list is empty");
+		}
+		E to_return = this.get(index);
+		
+		if(index == 0){
+			head = head.next;
+		}else{
+			Node<E> before = this.get_node(index - 1);
+			before.next = before.next.next;
+		}
+		
+		this.size--;
+		return to_return;
 	}
-	
+	public E remove(E item){
+		int index = this.exists(item);
+		if(index != -1){
+			E to_return = this.get(index);
+			this.remove(index);
+			return to_return;
+		}else{
+			return null;
+		}
+		
+	}
 	public E remove(){
-		return null;
+		return this.remove(this.size - 1);
 	}
 	
 	public String to_string(){
-		return "";
+		StringBuilder sb = new StringBuilder();
+		Node<E> temp = this.head;
+		while(temp != null){
+			sb.append(temp.item).append("-->");;
+			temp = temp.next;
+		}
+	    sb.append("Null");
+		return sb.toString();
 	}
 	
 
 	public static void main(String [] args){
+		Node<Integer> node = new Node<>(1);
+		SinglyLinkedList<Integer> foo = new SinglyLinkedList<>();
+		foo.add(1);
+		foo.add(3);
+		foo.add(4);
+		foo.remove();
+		
+		
+		System.out.println(foo.to_string());
 		
 	}
 }
